81183a96012869ec21184d1b3ab1e49c46ff8114
Rektroth <brian.rexroth.jr@gmail.com>
2024-11-15 00:30:08 -0500

    a LOT of design reworking

--- web/public/js/probs.js
+++ web/public/js/probs.js
@@ -1,13 +1,24 @@
 function changeProbs() {
     var select = document.querySelector("#probs");
-    var probsSeeding = document.querySelectorAll("#probs-seeding");
+    var probsMinSeeding = document.querySelectorAll("#probs-min-seeding");
+    var probsIndSeeding = document.querySelectorAll("#probs-ind-seeding");
     var probsPlayoffs = document.querySelectorAll("#probs-playoffs");
 
-    if (select.value == 'seeding') {
-        probsSeeding.forEach(p => p.classList.remove("probs-hidden"));
-        probsPlayoffs.forEach(p => p.classList.add("probs-hidden"));
-    } else {
-        probsSeeding.forEach(p => p.classList.add("probs-hidden"));
-        probsPlayoffs.forEach(p => p.classList.remove("probs-hidden"));
+    switch (select.value) {
+        case "min-seeding":
+            probsMinSeeding.forEach(p => p.classList.remove("probs-hidden"));
+            probsIndSeeding.forEach(p => p.classList.add("probs-hidden"));
+            probsPlayoffs.forEach(p => p.classList.add("probs-hidden"));
+            break;
+        case "ind-seeding":
+            probsMinSeeding.forEach(p => p.classList.add("probs-hidden"));
+            probsIndSeeding.forEach(p => p.classList.remove("probs-hidden"));
+            probsPlayoffs.forEach(p => p.classList.add("probs-hidden"));
+            break;
+        default:
+            probsMinSeeding.forEach(p => p.classList.add("probs-hidden"));
+            probsIndSeeding.forEach(p => p.classList.add("probs-hidden"));
+            probsPlayoffs.forEach(p => p.classList.remove("probs-hidden"));
+            break;
     }
 }
